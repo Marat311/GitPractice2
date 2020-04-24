535df63f24f1b99f98c7c0fd4fa8a7d2e0bd9b20
Marina Velitskaia <marinavelitskaia@gmail.com>
2020-04-24 15:52:47 -0400

    add Class2.java

---
+++ src/day_1/Class2.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.println("My name is Marina");
+        System.out.println("I'm from Russia");
+        System.out.println("I want have a lot of money");
+    }
+}
