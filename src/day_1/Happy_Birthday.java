e71db25545ec458cd6eb38267e9c544cd46f0a45
Marina Vel <64120658+Marat311@users.noreply.github.com>
2020-04-28 16:03:06 -0400

    Update Happy_Birthday.java

--- src/day_1/Happy_Birthday.java
+++ src/day_1/Happy_Birthday.java
@@ -4,5 +4,6 @@ public class Happy_Birthday {
     public static void main(String[] args) {
         System.out.println("Happy birthday");
         //soory im try
+        System.out.println("Hello");
     }
 }
